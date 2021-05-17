f3317085e23b59694419a5a675f43d07829ea1d0
Thone Cardoso de Araujo <thonecardoso@gmail.com>
2021-05-17 08:10:18 -0300

    diretiva ngFor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppComponent } from './app.component';
 import { DiretivaNgifComponent } from './diretiva-ngif/diretiva-ngif.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { DiretivaNgswitchComponent } from './diretiva-ngswitch/diretiva-ngswitch.component';
+import { DiretivaNgforComponent } from './diretiva-ngfor/diretiva-ngfor.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     DiretivaNgifComponent,
-    DiretivaNgswitchComponent
+    DiretivaNgswitchComponent,
+    DiretivaNgforComponent
   ],
   imports: [
     BrowserModule,
